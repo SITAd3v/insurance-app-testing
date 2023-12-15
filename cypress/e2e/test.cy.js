ed3446aeee79062e39ad563663dbb0ad829c31d1
Nidhi Singh <nidhi.into.pgt@gmail.com>
2023-12-15 10:44:28 +0100

    gurru99 login

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,12 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://google.com')
+    cy.visit('http://demo.guru99.com/insurance/v1/index.php')
+    //Accept cookies
+    cy.get('#email').type("nidhi.into.pgt@gmail.com")
+    cy.clear ("cookie")
+    cy.get('#password').type("nidhi@123")
+    cy.get(':nth-child(3) > .btn').click()
+    //Verify login
   })
 })
 //Attomation code
