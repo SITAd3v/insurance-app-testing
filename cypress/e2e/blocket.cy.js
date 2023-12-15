a92b2345e54668461f35fd6ab18e16212c06386b
Arpita16 <arpita16.4u@gmail.com>
2023-12-15 05:14:47 +0100

    click on login button

--- cypress/e2e/blocket.cy.js
+++ cypress/e2e/blocket.cy.js
@@ -2,7 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.blocket.se/')
     cy.get('#sp_message_iframe_960943').click()
-    cy.get('.react-autosuggest__input').type('Bilar')
+  
+    cy.get('.Profile__Wrapper-sc-exqh0k-0 > .StyledLink-sc-eac612-0').click()
    
     
 
